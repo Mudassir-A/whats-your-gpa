338c9814c9b9c75405828e771a21ea953ad0d798
Mohd Mudassir Ansari <mudassirimranansari@gmail.com>
2024-12-30 01:32:23 +0530

    GPA Prediction Model

--- src/model.py
+++ src/model.py
@@ -1,8 +1,5 @@
 from torch import nn
 
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import MinMaxScaler
-
 class GPAPredictor(nn.Module):
 	def __init__(self):
 		super().__init__()
